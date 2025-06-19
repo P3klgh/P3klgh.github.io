38c4888348c1a84e6c4b479cd31467c014ed1b3d
Kenneth Lee <n11532386@qut.edu.au>
2025-06-19 22:12:11 +1000

    Update GitHub Actions workflow and clean up project files

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/P3klgh-page' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/P3klgh.github.io' : '',
 };
 
 export default nextConfig;

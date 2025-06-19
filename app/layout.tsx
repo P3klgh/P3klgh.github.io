1c52017160f216183294f2e1fd1432f0e20179a4
Kenneth Lee <n11532386@qut.edu.au>
2025-06-19 23:04:27 +1000

    Add next-themes package for improved theming support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,13 +12,9 @@ export const metadata: Metadata = {
   authors: [{ name: "P3klgh" }],
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children,}: {children: React.ReactNode;}) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
@@ -33,8 +30,14 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
-        {children}
-        
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>  
         {/* Scripts */}
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
